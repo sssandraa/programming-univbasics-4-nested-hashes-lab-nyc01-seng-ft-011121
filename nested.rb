6cd2d66ffebbfcdf8340d0eda18bfcdbf20ab35c
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-02 00:00:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,5 +112,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-		programmer_hash[:dennis_ritchie][:languages][1].include "Assembly"
+		programmer_hash[:dennis_ritchie][:languages][1].to include "Assembly"
 end
